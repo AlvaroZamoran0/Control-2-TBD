d3740b3a9a72ad17fa704b773b2535e199afe884
Alvaro Z <alvaro.zamorano@usach.cl>
2024-11-15 21:20:41 -0300

    Backend

---
+++ demo/src/main/java/com/example/demo/Control2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control2Application.class, args);
+	}
+
+}
